af9ee4e4cd2b4959b31cb3a7b383a81aa7685021
Mojammel Haque <mojammel127@gmail.com>
2020-09-18 06:59:18 +0600

    Updates

---
+++ precache-manifest.86b3f3432d18c177a0788fab0940950a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "294226dd5a0b5e753538b6c891de5967",
+    "url": "/assignment-8/index.html"
+  },
+  {
+    "revision": "e713cddb55452090f393",
+    "url": "/assignment-8/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3c361c2ea3070a61c313",
+    "url": "/assignment-8/static/js/2.6d518c95.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/assignment-8/static/js/2.6d518c95.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e713cddb55452090f393",
+    "url": "/assignment-8/static/js/main.b7f964ed.chunk.js"
+  },
+  {
+    "revision": "b60f1448cf1ea747fc20",
+    "url": "/assignment-8/static/js/runtime-main.fecff10a.js"
+  }
+]);
